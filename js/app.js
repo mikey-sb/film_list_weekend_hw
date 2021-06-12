4951a60b759407b9e0bff06a264d91bc84f3d26d
michael403053 <michael.christie5@hotmail.co.uk>
2021-06-12 16:47:16 +0100

    updated ui and transitions

--- js/app.js
+++ js/app.js
@@ -23,17 +23,6 @@ const handleButton = function(event){
 
 const createFilmListItem = function (form) {
 const filmListItem = document.createElement('ul');
-const rating_val = form.rating.value
-    if(rating_val == 1){
-filmListItem.classList.add('one-star');}
-    if(rating_val == 2){
-filmListItem.classList.add('two-star');}
-    if(rating_val == 3){
-filmListItem.classList.add('three-star');}
-    if(rating_val == 4){
-filmListItem.classList.add('four-star');}
-    if(rating_val == 5){
-filmListItem.classList.add('five-star');}
 
 const title = document.createElement('li');
 title.textContent = form.title.value;
@@ -49,22 +38,22 @@ const genre = document.createElement('li');
 const genre_val = form.genre.value
 genre.textContent = form.genre.value;
 filmListItem.appendChild(genre);
-    if(genre_val == 'comedy'){
+    if(genre_val == 'Comedy'){
         filmListItem.classList.add('comedy')
     }
-    if(genre_val == 'action'){
+    if(genre_val == 'Action'){
         filmListItem.classList.add('action')
     }
-    if(genre_val == 'drama'){
+    if(genre_val == 'Drama'){
         filmListItem.classList.add('drama')
     }
-    if(genre_val == 'horror'){
+    if(genre_val == 'Horror'){
         filmListItem.classList.add('horror')
     }
-    if(genre_val == 'romance'){
+    if(genre_val == 'Romance'){
         filmListItem.classList.add('romance')
     }
-    if(genre_val == 'documentary'){
+    if(genre_val == 'Documentary'){
         filmListItem.classList.add('documentary')
     }
 
@@ -74,38 +63,37 @@ filmListItem.appendChild(year);
 year.classList.add('li-year')
 
 const rating = document.createElement('li');
-rating.textContent = form.rating.value;
 filmListItem.appendChild(rating);
+const rating_val = form.rating.value
+rating.classList.add('li-rating')
 
+    if(rating_val == 1){
+        filmListItem.classList.add('one-star');
+        const star1 = document.getElementById('one-star-star')
+        rating.appendChild(star1)}
 
+    if(rating_val == 2){
+        filmListItem.classList.add('two-star');
+        const star2 = document.getElementById('two-star-star')
+        rating.appendChild(star2)}
 
+    if(rating_val == 3){
+        filmListItem.classList.add('three-star');
+        const star3 = document.getElementById('three-star-star')
+        rating.appendChild(star3)}
 
-if(rating_val === 1){
-    console.log('HERE')
-formWrapper.classList.add('one-star')
-}
-if(rating_val === 2){
-formWrapper.classList.add('two-star')
-}
-if(rating_val === 3){
-formWrapper.classList.add('three-star')
-}
-if(rating_val === 4){
-formWrapper.classList.add('four-star')
-}
-if(rating_val === 5){
-formWrapper.classList.add('five-star')
-}
+    if(rating_val == 4){
+        filmListItem.classList.add('four-star');
+        const star4 = document.getElementById('four-star-star')
+        rating.appendChild(star4)}
 
-
-
-
+    if(rating_val == 5){      
+        filmListItem.classList.add('five-star');
+        const star5 = document.getElementById('five-star-star')
+        rating.appendChild(star5)}
 
 return filmListItem
 }
 
-const loadFile = function(event) {
-const image = document.getElementById('output');
-image.src = URL.createObjectURL(event.target.files[0]);
-};
+
 
